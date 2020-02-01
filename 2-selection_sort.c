e112ac868904670b145a50cb3df1c0ffd51ef8d3
amine784 <1066@holbertonschool.com>
2020-02-02 00:29:32 +0100

    may5demch kima yelzem

---
+++ 2-selection_sort.c
@@ -0,0 +1,32 @@
+#include <stdlib.h>
+#include <stdio.h>
+#include "sort.h"
+/**
+*selection_sort-function that sorts an array of integers in ascending
+*order using the Selection sort algorithm
+*@array: pointer to the first item
+*@size:size of the array
+*Return:Always success
+**/
+void selection_sort(int *array, size_t size)
+{
+size_t i, j = 0;
+size_t index;
+int min;
+for (i = 0; i < size - 1; i++)
+{
+index  = i;
+min  =  array[i];
+for (j = i + 1; j < size; j++)
+{
+if (min > array[j])
+{
+index = j;
+min = array[j];
+}
+}
+array[index] = array[i];
+array[i] = min;
+print_array(array, size);
+}
+}
