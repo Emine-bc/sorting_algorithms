6cb08ed3bc1e71d5b029fa48c452a0694665e7ef
amine784 <1066@holbertonschool.com>
2020-02-04 16:25:01 +0100

    advanced task

---
+++ 102-counting_sort.c
@@ -0,0 +1,41 @@
+#include "sort.h"
+/**
+ * counting_sort-a function that sorts an array of integers
+ * in ascending order using the Counting sort algorithm
+ *@array: pointer to the first item
+ *@size:size of the array
+ *Return:Always success
+ **/
+void counting_sort(int *array, size_t size)
+{
+size_t i;
+int max = array[0], x, al = 0, k;
+int *cmp = NULL, *copy = NULL;
+if (array == NULL)
+return;
+for (i = 0; i < size; i++)
+{
+if (array[i] > max)
+max = array[i];
+}
+copy = malloc(sizeof(int) * size);
+cmp = malloc(sizeof(int) * (max + 1));
+if (cmp == NULL || copy == NULL)
+return;
+for (i = 0; i < size; i++)
+cmp[array[i]] += 1;
+for (k = 0; k <= max; k++)
+{
+x = cmp[k];
+cmp[k] += al;
+al += x;
+}
+print_array(cmp, max + 1);
+for (i = 0; i < size; i++)
+{
+copy[cmp[array[i]] - 1] = array[i];
+cmp[array[i]] -= 1;
+}
+for (i = 0; i < size; i++)
+array[i] = copy[i];
+}
